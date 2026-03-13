5304491829cf28ce237fa152c65d0e645b6a50b1
Abd AlRhamn Faras <aa3819341@gmail.com>
2026-03-13 22:04:10 +0300

    Fix All Song & Artist Resource

--- app/Http/Resources/ArtistResource.php
+++ app/Http/Resources/ArtistResource.php
@@ -17,9 +17,9 @@ class ArtistResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'description' => $this->when($this->description, $this->description),
-            'birth_date' => $this->when($this->birth_date, $this->birth_date),
-            'country' => $this->when($this->country, $this->country),
+            'description' => $this->description,
+            'birth_date' => $this->birth_date,
+            'country' => $this->country,
             'create_at' => $this->created_at->format('Y-m-d : H:m'),
             'update_at' => $this->updated_at->format('Y-m-d : H:m'),
             'songs' => SongResource::collection($this->whenLoaded('songs')),
--- app/Http/Resources/SongResource.php
+++ app/Http/Resources/SongResource.php
@@ -17,10 +17,10 @@ class SongResource extends JsonResource
         return [
             'id' => $this->id,
             'title' => $this->title,
-            'description' => $this->when($this->description, $this->description),
-            'duration' => $this->when($this->duration, $this->duration),
-            'cover' => $this->when($this->cover, $this->cover),
-            'file_path' => $this->when($this->file_path, $this->file_path),
+            'description' => $this->description,
+            'duration' => $this->duration,
+            'cover' => $this->cover,
+            'file_path' => $this->file_path,
 
             'create_at' => $this->created_at->format('Y-m-d : H:m'),
             'update_at' => $this->updated_at->format('Y-m-d : H:m'),
